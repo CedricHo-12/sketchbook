17992a83842642bec449939f3a5783960330a599
CedricHo-12 <seefoong12@gmail.com>
2023-03-03 10:44:57 +0900

    CHANGE CURSOR COLOR AND CREATE RESET BUTTON
    black and eraser done created, rainbow color currently pink color

--- script.js
+++ script.js
@@ -8,9 +8,75 @@ function createGrid(gridNumber){
         container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
         container.insertAdjacentElement('beforeend', gridBox);
-
-
     }
 }
 
 createGrid(20);
+
+
+const smallButton = document.getElementById('small');
+const mediumButton = document.getElementById('medium');
+const largeButton = document.getElementById('large');
+
+smallButton.addEventListener('click' ,() => {
+    removeGrid();
+    createGrid(30);
+    
+}); 
+
+mediumButton.addEventListener('click' ,() => {
+    removeGrid();
+    createGrid(20);
+    
+}); 
+
+largeButton.addEventListener('click' ,() => {
+    removeGrid();
+    createGrid(10);
+   
+}); 
+
+
+function removeGrid() {
+    const gridBox = document.querySelectorAll('#container div');
+    gridBox.forEach((gridBox) => gridBox.remove());
+}
+
+//change curser color
+function draw(color){
+const gridBox = document.querySelectorAll('.box');
+gridBox.forEach( (gridBox) => {
+    gridBox.addEventListener('mouseover', () => {
+        if( color == 'black'){
+            gridBox.style.backgroundColor = 'black';
+        }else if(color == 'eraser'){
+            gridBox.style.backgroundColor = 'white';
+        }else if(color == 'rainbow'){
+            gridBox.style.backgroundColor = 'pink';
+        }
+    })
+});
+}
+
+
+const blackButton = document.getElementById('black');
+const eraserButton = document.getElementById('eraser');
+const rainbowButton = document.getElementById('rainbow');
+
+
+blackButton.addEventListener('click', () => draw('black'));
+eraserButton.addEventListener('click', () => draw('eraser'));
+rainbowButton.addEventListener('click', () => draw('rainbow'));
+
+
+
+//reset button
+const reset = document.getElementById('reset');
+reset.addEventListener('click', () => {
+    const resetColor = document.querySelectorAll('.box');
+    resetColor.forEach((box) => {
+        box.style.backgroundColor = 'white';
+    })
+})
+
+
